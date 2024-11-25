4a41b3e68de26507acd83361622ab8555ab58e01
PetoKrisa <petokrisa2006@gmail.com>
2024-11-25 11:48:00 +0100

    adatbázis és backend létrehozva

---
+++ tools/database_init.sql
@@ -0,0 +1,78 @@
+-- Active: 1732526766833@@127.0.0.1@3306@projekt2425
+CREATE DATABASE projekt2425;
+USE projekt2425;
+
+CREATE Table city (
+id int primary key unique AUTO_INCREMENT,
+name varchar(100),
+county varchar(100)
+);
+
+CREATE Table category(
+id int primary key unique auto_increment,
+name varchar(30),
+description varchar(255)
+)
+
+CREATE Table user(
+id int primary key unique auto_increment not null,
+username varchar(30) unique null,
+email varchar(100) unique not null,
+password varchar(255) null,
+join_date date default now(),
+city_id int,
+bio varchar(255),
+pfp varchar(255),
+completed int,
+role varchar(12),
+Foreign Key (city_id) REFERENCES city(id)
+)
+
+CREATE Table event(
+id int primary key unique auto_increment,
+title varchar(100),
+description varchar(1000),
+start_date datetime not null,
+end_date datetime null,
+visibility varchar(10) DEFAULT "public",
+cover varchar(255),
+city_id int,
+location varchar(255),
+max_response int null,
+age_limit BOOLEAN,
+date datetime not null default NOW(),
+Foreign Key (city_id) REFERENCES city(id)
+);
+
+CREATE table EventGalleryImage(
+    event_id int,
+    image varchar(500),
+    Foreign Key (event_id) REFERENCES event(id)
+);
+
+CREATE TABLE eventCategory(
+category_id int,
+event_id int,
+Foreign Key (category_id) REFERENCES category(id) on delete CASCADE,
+Foreign Key (event_id) REFERENCES event(id) on delete CASCADE
+);
+
+CREATE Table eventUser(
+user_id int,
+event_id int,
+type VARCHAR(20),
+date datetime default NOW(),
+Foreign Key (user_id) REFERENCES user(id) on delete CASCADE,
+Foreign Key (event_id) REFERENCES event(id) on delete CASCADE
+);
+
+CREATE Table eventComment(
+id int primary key unique auto_increment,
+event_id int,
+user_id int,
+comment_text varchar(255) not null,
+super_comment_id int null DEFAULT null,
+date datetime not null default NOW(),
+Foreign Key (user_id) REFERENCES user(id) on delete CASCADE,
+Foreign Key (event_id) REFERENCES event(id) on delete CASCADE
+)
